7f3b3737dd1d55353243e91171275f240d102847
Downes-michael <118235731+Downes-michael@users.noreply.github.com>
2023-09-28 10:06:12 -0500

    changed the title

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Downes Week 5'),
+      //Changed name
+      home: const MyHomePage(title: 'Week 6 Git'),
     );
   }
 }
